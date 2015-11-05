a5e3939435ccf897c019e57ea079a2d3188d65bd
Dvoretskiy Sergey <sidvoretskiy@gmail.com>
2015-11-06 00:20:16 +0300

    Виды и вложенные ресурсы

--- my_habr/app/models/category.rb
+++ my_habr/app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   #has_and_belongs_to_many :posts
-  belongs_to :categories_posts
+  has_many :categories_posts
   has_many :posts, through: :categories_posts
 
 
