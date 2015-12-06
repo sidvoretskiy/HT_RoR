1d82a9d6f09416c94ec2685bdc66968e02e42c2d
Dvoretskiy Sergey <sidvoretskiy@gmail.com>
2015-12-06 19:52:07 +0300

    e-mail server conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'thawing-fjord-7143.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'thawing-fjord-7143.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       port: ENV['MAILGUN_SMTP_PORT'],
