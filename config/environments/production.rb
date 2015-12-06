c4fcc440b9f1b430010512551b7d9acc68568dc1
Dvoretskiy Sergey <sidvoretskiy@gmail.com>
2015-12-06 19:42:51 +0300

    e-mail server conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       port: ENV['MAILGUN_SMTP_PORT'],
       address: ENV['MAILGUN_SMTP_SERVER'],
-      user_name: ENV['MAILGUN_SMTP_USER'],
+      user_name: ENV['MAILGUN_SMTP_LOGIN'],
       password: ENV['MAILGUN_SMTP_PASSWORD'],
       domain: ENV['thawing-fjord-7143.herokuapp.com'],
       authentication: :plain
